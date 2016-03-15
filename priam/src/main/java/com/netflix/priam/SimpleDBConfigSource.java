837523e68022068f8caaf8a851c3cd5fcc6d6cbb
jhuffman <james.huffman@proquest.com>
2016-03-15 13:35:07 -0400

    first try at Priam SSL changes

--- priam/src/main/java/com/netflix/priam/SimpleDBConfigSource.java
+++ priam/src/main/java/com/netflix/priam/SimpleDBConfigSource.java
@@ -5,8 +5,13 @@ import com.amazonaws.services.simpledb.model.Attribute;
 import com.amazonaws.services.simpledb.model.Item;
 import com.amazonaws.services.simpledb.model.SelectRequest;
 import com.amazonaws.services.simpledb.model.SelectResult;
+import com.amazonaws.services.simpledb.model.GetAttributesRequest;
+import com.amazonaws.services.simpledb.model.GetAttributesResult;
+import com.amazonaws.services.simpledb.model.PutAttributesRequest;
+import com.amazonaws.services.simpledb.model.ReplaceableAttribute;
 import com.google.common.base.Preconditions;
 import com.google.common.collect.Maps;
+import com.netflix.priam.utils.SystemUtils;
 import org.apache.commons.lang.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -31,9 +36,18 @@ import java.util.Map;
 public final class SimpleDBConfigSource extends AbstractConfigSource 
 {
     private static final Logger logger = LoggerFactory.getLogger(SimpleDBConfigSource.class.getName());
+    private final String INSTANCE_ID = SystemUtils.getDataFromUrl("http://169.254.169.254/latest/meta-data/instance-id").trim();
+    private static String APP_ID = "appId"; // ASG
+    private static String PROPERTY = "property";
+    private static String PROPERTY_VALUE = "value";
+    private static String REGION = "region";
+    private static String KEYSTORE = "keystore";
+    private static String ITEMNAME = "itemName";
 
     private static final String DOMAIN = "PriamProperties";
-    private static String ALL_QUERY = "select * from " + DOMAIN + " where " + Attributes.APP_ID + "='%s'";
+    private static String ALL_QUERY = "select * from " + DOMAIN + " where " + APP_ID + "='%s'";
+    public static final String DOMAIN_SECURITY = "InstanceSecurity";
+    private static String SECURITY_QUERY = "select itemName, keystore, instanceId, updateTimestamp, truststore from " + DOMAIN_SECURITY + " where " + APP_ID + "='%s'";
 
     private final Map<String, String> data = Maps.newConcurrentMap();
     private final ICredential provider;
@@ -67,14 +81,48 @@ public final class SimpleDBConfigSource extends AbstractConfigSource
 
         } 
         while (nextToken != null);
-    }
 
-    private static class Attributes 
-    {
-        public final static String APP_ID = "appId"; // ASG
-        public final static String PROPERTY = "property";
-        public final static String PROPERTY_VALUE = "value";
-        public final static String REGION = "region";
+        // read in additional info from simpleDB
+        System.out.println("Start of new SimpleDB section");
+
+        nextToken = null;
+        do
+        {
+            SelectRequest request = new SelectRequest(String.format(SECURITY_QUERY, appid));
+            request.setNextToken(nextToken);
+            SelectResult result = simpleDBClient.select(request);
+            nextToken = result.getNextToken();
+            Iterator<Item> itemiter = result.getItems().iterator();
+            System.out.println("SimpleDB2");
+            while (itemiter.hasNext()) {
+                System.out.println("SimpleDB3");
+                addProperty(itemiter.next());
+
+
+                String ks = data.get(KEYSTORE);
+
+                if(ks == null) {
+                    System.out.println("SimpleDB4");
+                     // this keystore may be available
+                     // try to update this row with the instanceId
+                      // pi.setKeystore(iid);
+                     PutAttributesRequest req = new PutAttributesRequest()
+                         .withDomainName(DOMAIN_SECURITY)
+                         .withItemName(data.get(ITEMNAME))
+                         .withAttributes(new ReplaceableAttribute("instanceId", INSTANCE_ID, false));
+                     simpleDBClient.putAttributes(req);
+                    System.out.println("SimpleDB5");
+                  GetAttributesRequest getReq = new GetAttributesRequest(DOMAIN_SECURITY, "instanceId").withConsistentRead(true);
+                     GetAttributesResult attResult = simpleDBClient.getAttributes(getReq);
+
+                  for (Attribute attr : attResult.getAttributes()) {
+                         System.out.println("name: " + attr.getName() + "\tvalue: " + attr.getValue());
+                     } // for
+                 } // if
+            } // while more rows
+            System.out.println("SimpleDB6");
+        } while (nextToken != null);
+        System.out.println("SimpleDB7");
     }
 
     private void addProperty(Item item) 
@@ -86,11 +134,11 @@ public final class SimpleDBConfigSource extends AbstractConfigSource
         while (attrs.hasNext()) 
         {
             Attribute att = attrs.next();
-            if (att.getName().equals(Attributes.PROPERTY))
+            if (att.getName().equals(PROPERTY))
                 prop = att.getValue();
-            else if (att.getName().equals(Attributes.PROPERTY_VALUE))
+            else if (att.getName().equals(PROPERTY_VALUE))
                 value = att.getValue();
-            else if (att.getName().equals(Attributes.REGION))
+            else if (att.getName().equals(REGION))
                 dc = att.getValue();
         }
         // Ignore, if not this region
@@ -101,6 +149,8 @@ public final class SimpleDBConfigSource extends AbstractConfigSource
             return;
         data.put(prop, value);
     }
+
+
 
     @Override
     public int size() 
